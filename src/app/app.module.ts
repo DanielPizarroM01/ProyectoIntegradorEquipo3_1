12becf4cf5a25aef5afdb48372e9e85888eee4c2
DanielPizarroM01 <daniel_2001_eduardo@hotmail.com>
2024-11-23 01:10:34 -0600

    Actualización

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule,  CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"proyecto-integrador-2b376","appId":"1:583949730233:web:9b2dc1af6bd28c89a27668","storageBucket":"proyecto-integrador-2b376.firebasestorage.app","apiKey":"AIzaSyDMY_4NmiQoAd4yTHGRBXDePjaAOdsVxmM","authDomain":"proyecto-integrador-2b376.firebaseapp.com","messagingSenderId":"583949730233","measurementId":"G-1682TY1FSN"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
